1401f967b4e659056f51b85066f570daffdb40ea
Blake Gentry <blakesgentry@gmail.com>
2015-11-19 10:26:44 -0800

    replace 'protected' route with 'library' route

--- tests/unit/routes/library-test.js
+++ tests/unit/routes/library-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:protected', 'Unit | Route | protected', {
+moduleFor('route:library', 'Unit | Route | library', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
