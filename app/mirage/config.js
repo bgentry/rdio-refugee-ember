d47877bba215ba5cf75647914d6f5e1503db1545
Blake Gentry <blakesgentry@gmail.com>
2015-11-18 21:18:06 -0800

    basic ember-simple-auth, ember-cli-mirage for it

---
+++ app/mirage/config.js
@@ -0,0 +1,98 @@
+import Mirage from 'ember-cli-mirage';
+
+export default function() {
+
+  // These comments are here to help you get started. Feel free to delete them.
+
+  /*
+    Config (with defaults).
+
+    Note: these only affect routes defined *after* them!
+  */
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Route shorthand cheatsheet
+  */
+  /*
+    GET shorthands
+
+    // Collections
+    this.get('/contacts');
+    this.get('/contacts', 'users');
+    this.get('/contacts', ['contacts', 'addresses']);
+
+    // Single objects
+    this.get('/contacts/:id');
+    this.get('/contacts/:id', 'user');
+    this.get('/contacts/:id', ['contact', 'addresses']);
+  */
+
+  /*
+    POST shorthands
+
+    this.post('/contacts');
+    this.post('/contacts', 'user'); // specify the type of resource to be created
+  */
+
+  /*
+    PUT shorthands
+
+    this.put('/contacts/:id');
+    this.put('/contacts/:id', 'user'); // specify the type of resource to be updated
+  */
+
+  /*
+    DELETE shorthands
+
+    this.del('/contacts/:id');
+    this.del('/contacts/:id', 'user'); // specify the type of resource to be deleted
+
+    // Single object + related resources. Make sure parent resource is first.
+    this.del('/contacts/:id', ['contact', 'addresses']);
+  */
+
+  /*
+    Function fallback. Manipulate data in the db via
+
+      - db.{collection}
+      - db.{collection}.find(id)
+      - db.{collection}.where(query)
+      - db.{collection}.update(target, attrs)
+      - db.{collection}.remove(target)
+
+    // Example: return a single object with related models
+    this.get('/contacts/:id', function(db, request) {
+      var contactId = +request.params.id;
+
+      return {
+        contact: db.contacts.find(contactId),
+        addresses: db.addresses.where({contact_id: contactId})
+      };
+    });
+
+  */
+  this.post('/token', function(db, req) {
+    // TODO: find a way to decode req body form data so I don't have to hack includes checks
+    const body = req.requestBody;
+    if (body.grant_type === 'password' || body.includes('grant_type=password')) {
+      //if (body.username === 'letme' && body.password === 'in') {
+      if (body.includes('username=letme') && body.includes('password=in')) {
+        return { access_token: 'secret token' };
+      } else {
+        return new Mirage.Response(400, {}, { error: 'invalid_grant' });
+      }
+    } else {
+      return new Mirage.Response(400, {}, { error: 'unsupported_grant_type' });
+    }
+  });
+}
+
+/*
+You can optionally export a config that is only loaded during tests
+export function testConfig() {
+
+}
+*/
