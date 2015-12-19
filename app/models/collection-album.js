8279304e64182b81b9eae5f5e2ed1ea4931d5d25
Blake Gentry <blakesgentry@gmail.com>
2015-12-18 21:29:53 -0800

    rename match to album-match

--- app/models/collection-album.js
+++ app/models/collection-album.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 const { attr, hasMany } = DS;
 
 export default DS.Model.extend({
-  matches: hasMany('matches'),
+  matches: hasMany('albumMatches'),
   rdioAlbum: attr(),
   selectedMatchId: attr('number'),
 
--- tests/unit/models/album-match-test.js
+++ tests/unit/models/album-match-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('match', 'Unit | Model | match', {
+moduleForModel('albumMatch', 'Unit | Model | match', {
   // Specify the other units that are required for this test.
   needs: []
 });
