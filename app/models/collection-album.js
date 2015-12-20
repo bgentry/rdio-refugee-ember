9fcccfc384a1111ccd1b6b517c2af887cee09bbb
Blake Gentry <blakesgentry@gmail.com>
2015-12-19 21:52:47 -0800

    track count for rdio album

--- app/models/collection-album.js
+++ app/models/collection-album.js
@@ -1,10 +1,17 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+const { computed } = Ember;
 const { attr, hasMany } = DS;
 
 export default DS.Model.extend({
   matches: hasMany('albumMatches'),
   rdioAlbum: attr(),
   selectedMatchId: attr('string'),
+
+  rdioTrackCount: computed('rdioAlbum', function() {
+    const rdioAlbum = this.get('rdioAlbum');
+    return rdioAlbum.tracks.length;
+  }),
 
   createdAt: attr('date'),
   updatedAt: attr('date')
