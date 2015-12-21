1a20b9e18c9e336be48323c7570617f824a11dc9
Blake Gentry <blakesgentry@gmail.com>
2015-12-21 00:20:26 -0800

    duration format helper

---
+++ tests/unit/helpers/duration-test.js
@@ -0,0 +1,10 @@
+import { momentDurationFormat } from '../../../helpers/moment-duration-format';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | moment duration format');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = momentDurationFormat(42);
+  assert.ok(result);
+});
