0649f92c33e7f6e5d0f1a64b2bdbb0a11955cd68
Blake Gentry <blakesgentry@gmail.com>
2015-12-21 13:56:28 -0800

    track matches

---
+++ app/components/track-match.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+const { computed } = Ember;
+
+export default Ember.Component.extend({
+  classNames: ['spotify-track'],
+
+  actions: {
+    selectMatch() {
+      const collectionTrack = this.get('collectionTrack');
+      const matchId = this.get('match.id');
+
+      function saveSuccess() {
+        console.log('selected match saved!');
+      }
+
+      function saveFailure(reason) {
+        console.log('selected match save failed!', reason);
+      }
+
+      collectionTrack.set('selectedMatchId', matchId);
+      collectionTrack.save().then(saveSuccess).catch(saveFailure);
+    }
+  },
+
+  // properties:
+
+  isSelected: computed('collectionTrack.selectedMatchId', 'match.id', function() {
+    const selectedMatchId = this.get('collectionTrack.selectedMatchId');
+    // hack because I couldn't figure out how to get this ID to always be an int :(
+    const thisMatchId = parseInt(this.get('match.id'));
+    return selectedMatchId === thisMatchId;
+  })
+});
+
